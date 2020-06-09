38dce54f8b450415ba49237e6506f3409539ce5b
DarkcrusherX <ahalmartinv@gmail.com>
2020-06-09 23:00:15 +0530

    arm function

--- src/arm0.py
+++ src/arm0.py
@@ -3,67 +3,92 @@ import mavros
 from geometry_msgs.msg import PoseStamped
 from mavros_msgs.msg import State 
 from mavros_msgs.srv import CommandBool, SetMode
+#rospy.init_node('arm_and_takeoff_node', anonymous=True)
+class armtakeoff():
 
-# callback method for state sub
-current_state = State() 
-offb_set_mode = SetMode
-def state_cb(state):
-    global current_state
-    current_state = state
+    def __init__(self):
 
-local_pos_pub = rospy.Publisher('/uav0/mavros/setpoint_position/local', PoseStamped, queue_size=10)
-state_sub = rospy.Subscriber('/uav0/mavros/state', State, state_cb)  # $This topic was wrong
-arming_client = rospy.ServiceProxy('/uav0/mavros/cmd/arming', CommandBool)
-set_mode_client = rospy.ServiceProxy('/uav0/mavros/set_mode', SetMode) 
+        def state_cb(state):
+            self.current_state = state
+        
+        self.current_state = State()
+        self.local_pos_pub = rospy.Publisher('/uav0/mavros/setpoint_position/local', PoseStamped, queue_size=10)
+        self.state_sub = rospy.Subscriber('/uav0/mavros/state', State, state_cb)  # $This topic was wrong
+        self.arming_client = rospy.ServiceProxy('uav0/mavros/cmd/arming', CommandBool)
+        self.set_mode_client = rospy.ServiceProxy('/uav0/mavros/set_mode', SetMode)
+     
+    
+    def arm(self):
 
-pose = PoseStamped()
-pose.pose.position.x = 0
-pose.pose.position.y = 0
-pose.pose.position.z = 5
-
-def position_control():
-    rospy.init_node('offb_node', anonymous=True)
-    prev_state = current_state
-    rate = rospy.Rate(20.0) # MUST be more then 2Hz
-
-    # send a few setpoints before starting
-    for i in range(100):
-        local_pos_pub.publish(pose)
-        rate.sleep()
-    print(current_state)
-    # wait for FCU connection
-    while not current_state.connected:
-        print("Sleeping")
-        rate.sleep()
-
-    last_request = rospy.get_rostime()
-    while not rospy.is_shutdown():
-        print("Got in")
-        now = rospy.get_rostime()
-        if current_state.mode != "OFFBOARD" and (now - last_request > rospy.Duration(5.)):
-            set_mode_client(base_mode=0, custom_mode="OFFBOARD")
-            last_request = now 
-        else:
-            if not current_state.armed and (now - last_request > rospy.Duration(5.)):
-               arming_client(True)
-               last_request = now 
-
-        # older versions of PX4 always return success==True, so better to check Status instead
-        if prev_state.armed != current_state.armed:
-            rospy.loginfo("Vehicle armed: %r" % current_state.armed)
-        if prev_state.mode != current_state.mode: 
-            rospy.loginfo("Current mode: %s" % current_state.mode)
-        prev_state = current_state
-
-        # Update timestamp and publish pose 
-        pose.header.stamp = rospy.Time.now()
+        pose = PoseStamped()
         pose.pose.position.x = 0
-        local_pos_pub.publish(pose)
-        rate.sleep()
+        pose.pose.position.y = 0
+        pose.pose.position.z = 5
 
-# $$Changed this
-if __name__ == "__main__":
-    try:
-        position_control()
-    except rospy.ROSInterruptException:
-        pass
+        prev_state = self.current_state
+        rate = rospy.Rate(20.0) # MUST be more then 2Hz
+
+        # send a few setpoints before starting
+        for i in range(100):
+            self.local_pos_pub.publish(pose)
+        rate.sleep()
+        print(self.current_state)
+        # wait for FCU connection
+        while not self.current_state.connected:
+            print("Sleeping")
+            rate.sleep()
+
+        last_request = rospy.get_rostime()
+
+        while (self.current_state.armed != True) or (self.current_state.mode !="OFFBOARD"): 
+        #    print("Arm {}" .format(self.current_state.armed))            
+            now = rospy.get_rostime()
+            if self.current_state.mode != "OFFBOARD" and (now - last_request > rospy.Duration(5.)):
+                self.set_mode_client(base_mode=0, custom_mode="OFFBOARD")
+                last_request = now 
+            else:
+                if not self.current_state.armed and (now - last_request > rospy.Duration(5.)):
+                    self.arming_client(True)
+                    last_request = now 
+
+            # older versions of PX4 always return success==True, so better to check Status instead
+            if prev_state.armed != self.current_state.armed:
+                rospy.loginfo("Vehicle armed: %r" % self.current_state.armed)
+            if prev_state.mode != self.current_state.mode: 
+                rospy.loginfo("Current mode: %s" % self.current_state.mode)
+            prev_state = self.current_state
+
+            # Update timestamp and publish pose 
+            pose.header.stamp = rospy.Time.now()
+            pose.pose.position.x = 0
+            if prev_state.armed == self.current_state.armed:
+                for i in range(100):
+                    self.local_pos_pub.publish(pose)     
+            rate.sleep()
+
+    def disarm(self):
+
+        prev_state = self.current_state
+        rate = rospy.Rate(20.0) # MUST be more then 2Hz
+
+        rate.sleep()
+        print(self.current_state)
+        # wait for FCU connection
+        while not self.current_state.connected:
+            print("Sleeping")
+            rate.sleep()
+
+        last_request = rospy.get_rostime()
+
+        while self.current_state.armed != False: 
+            #print("Got in")
+        #    print("Arm {}" .format(self.current_state.armed))
+            now = rospy.get_rostime()
+            if self.current_state.mode != "AUTO.LAND" and (now - last_request > rospy.Duration(5.)):
+                self.set_mode_client(base_mode=0, custom_mode="AUTO.LAND")
+                last_request = now 
+            else:
+                if not self.current_state.armed and (now - last_request > rospy.Duration(5.)):
+                    self.arming_client(False)
+                    last_request = now 
+        rate.sleep()            
